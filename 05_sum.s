e24c1a95a0f0626a7957aa1204e9ddfa7884ec8c
sshwy <jy.cat@qq.com>
2023-07-10 23:18:45 +0800

    generate 05_sum.s

---
+++ 05_sum.s
@@ -0,0 +1,21 @@
+	.file	"05_sum.c"
+	.text
+	.globl	sumstore
+	.type	sumstore, @function
+sumstore:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdx, %rbx
+	call	plus@PLT
+	movq	%rax, (%rbx)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sumstore, .-sumstore
+	.ident	"GCC: (GNU) 13.1.1 20230429"
+	.section	.note.GNU-stack,"",@progbits
